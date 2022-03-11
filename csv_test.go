880d47ec931dc58c9b5ee85b06d5dddbf86265fc
jiro4989 <jiroron666@gmail.com>
2022-03-12 08:04:39 +0900

    add test code

--- csv_test.go
+++ csv_test.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"io"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestCSV_Read(t *testing.T) {
+	tests := []struct {
+		desc    string
+		r       io.Reader
+		lf      string
+		want    []string
+		wantErr bool
+	}{
+		{
+			desc:    "正常系: カラムごとに取得できる",
+			r:       strings.NewReader("a,b,c"),
+			lf:      "\n",
+			want:    []string{"a", "b", "c"},
+			wantErr: false,
+		},
+		{
+			desc:    "正常系: カンマが混在",
+			r:       strings.NewReader(`"a,b","c"`),
+			lf:      "\n",
+			want:    []string{`a,b`, `c`},
+			wantErr: false,
+		},
+		{
+			desc:    "正常系: 改行文字",
+			r:       strings.NewReader("\"a\nb\",c,\"d\ne\""),
+			lf:      "\n",
+			want:    []string{`a\nb`, "c", `d\ne`},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			assert := assert.New(t)
+
+			c := NewCSV(tt.r, tt.lf)
+			got, err := c.ReadFold()
+			if tt.wantErr {
+				assert.Error(err)
+				assert.Nil(got)
+				return
+			}
+
+			assert.NoError(err)
+			assert.Equal(tt.want, got)
+		})
+	}
+}
 
 func TestFold(t *testing.T) {
 	tests := []struct {
